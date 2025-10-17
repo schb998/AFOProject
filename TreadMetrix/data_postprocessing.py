8403d9381e99ed90324d7820700a19712c11b772
lgre690 <lilas.grenier@etu.univ-lyon1.fr>
2025-10-17 16:31:06 +1300

    addition of trc segmentation

--- TreadMetrix/data_postprocessing.py
+++ TreadMetrix/data_postprocessing.py
@@ -1,10 +1,13 @@
 from resources.filetypes_gestion.mot import MOT
-
+from resources.filetypes_gestion.trc import TRC
 import os
 from copy import deepcopy
 from scipy.signal import butter, filtfilt, find_peaks
 import matplotlib.pyplot as plt
 import numpy as np
+
+# todo: join heelstrike + toeoff function for faster results
+
 
 def filter_grf(mot, fs):
     """Filters data of a MOT object with a Butterworth filter.
@@ -13,15 +16,16 @@ def filter_grf(mot, fs):
         mot  (MOT): MOT object whose data is to be filtered.
         fs (float): sampling frequency.
     """
-    b, a        = butter(6, 12 / (fs / 2), btype='low')
+    b, a = butter(6, 12 / (fs / 2), btype='low')
     filtered_df = deepcopy(mot.data)
     for col in mot.data.columns.tolist():
         filtered_df[col] = filtfilt(b, a, mot.data[col])
     mot.data = filtered_df
     return None
 
+
 def baseline_correct_debug(mot, fz_col, related_cols, output_path, show=False):
-    """Corrects the baseline of one of the columns of the m data.
+    """Corrects the baseline of one of the columns of the mot data.
 
     Saves plot of the baseline correction.
 
@@ -32,18 +36,20 @@ def baseline_correct_debug(mot, fz_col, related_cols, output_path, show=False):
         output_path          (string): output path for plot save.
         show                   (bool): whether to show the figure when method is called.
     """
-    fy                = mot.data[fz_col]
-    corrected_df      = deepcopy(mot.data)
+    fy = mot.data[fz_col]
+    corrected_df = deepcopy(mot.data)
     valley_indices, _ = find_peaks(-fy)
-    swing_valleys     = valley_indices[fy[valley_indices] < 0]
+    swing_valleys = valley_indices[fy[valley_indices] < 0]
 
     print(f"\nCorrecting {fz_col}")
     print(f"Number of swing valleys below 0N: {len(swing_valleys)}")
 
     time_scale = mot.data['time'] if 'time' in mot.data.columns.tolist() else np.arange(len(fy))
+    """ # commented for debug  # 
     plt.figure(figsize=(12, 4))
     plt.plot(time_scale, fy, label='Original', alpha=0.7)
     plt.scatter(time_scale[swing_valleys], fy[swing_valleys], color='red', label='Swing Valleys')
+    """  #
 
     if len(swing_valleys) == 0:
         print("No valleys found below zero. Skipping correction.")
@@ -54,23 +60,28 @@ def baseline_correct_debug(mot, fz_col, related_cols, output_path, show=False):
     corrected_df[fz_col] = fy + baseline
 
     for col in related_cols:
-        related           = mot.data[col]
-        offset            = np.median(related[swing_valleys])
+        related = mot.data[col]
+        offset = np.median(related[swing_valleys])
         corrected_df[col] = related - offset if offset > 0 else related + abs(offset)
         print(f"Offset for {col}: {offset:.2f}")
 
+    mot.data = corrected_df
+    """ # commented for debug #   
     plt.plot(time_scale, corrected_df[fz_col], label='Corrected', alpha=0.8)
     plt.title(f"{fz_col} Baseline Correction")
     plt.xlabel("Time [s]")
     plt.ylabel("Force [N]")
     plt.legend()
     plt.grid(True)
-    plt.savefig(os.path.join(output_path, f"{ mot.filename.replace('.mot', '') }_baseline_correction_{fz_col}.png"),
-                bbox_inches='tight')
-    mot.data = corrected_df
+    os.makedirs(output_path, exist_ok=True)
+    plt.savefig(os.path.join(output_path, f"{ mot.filename.replace('.mot', '') }_baseline_correction_{fz_col}.png"), 
+                            bbox_inches='tight')
+    
     if show:
         plt.show()
+    """#
     return None
+
 
 def detect_toe_offs(zeroed_mot, fs, threshold=20):
     """Detects the gait cycles' "toe off" points.
@@ -87,11 +98,11 @@ def detect_toe_offs(zeroed_mot, fs, threshold=20):
     toe_offs = {'R': [], 'L': []}
 
     if 'ground_force2_vy' in zeroed_mot.data.columns.tolist():
-        rzf          = zeroed_mot.data['ground_force2_vy']
+        rzf = zeroed_mot.data['ground_force2_vy']
         r_indexes, _ = find_peaks(rzf, prominence=15, distance=int(fs / 10), height=200)
         peak = 0
         while peak < len(r_indexes):
-            idx_start    = r_indexes[peak]
+            idx_start = r_indexes[peak]
             below_thresh = np.where(rzf[idx_start:] < threshold)[0]
             if len(below_thresh) == 0:
                 break
@@ -102,11 +113,11 @@ def detect_toe_offs(zeroed_mot, fs, threshold=20):
             peak = next_peaks[0]
 
     if 'ground_force1_vy' in zeroed_mot.data.columns.tolist():
-        lzf          = zeroed_mot.data['ground_force1_vy']
+        lzf = zeroed_mot.data['ground_force1_vy']
         l_indexes, _ = find_peaks(lzf, prominence=15, distance=int(fs / 10), height=200)
         peak = 0
         while peak < len(l_indexes):
-            idx_start    = l_indexes[peak]
+            idx_start = l_indexes[peak]
             below_thresh = np.where(lzf[idx_start:] < threshold)[0]
             if len(below_thresh) == 0:
                 break
@@ -117,6 +128,7 @@ def detect_toe_offs(zeroed_mot, fs, threshold=20):
             peak = next_peaks[0]
 
     return toe_offs
+
 
 def detect_heel_strikes(zeroed_mot, fs, threshold=20):
     """Detects the gait cycles' "heel strikes" points.
@@ -130,19 +142,19 @@ def detect_heel_strikes(zeroed_mot, fs, threshold=20):
 
     """
     heel_contacts = {'R': [], 'L': []}
-    distance      = int(fs / 2)
+    distance = int(fs / 2)
 
     if 'ground_force2_vy' in zeroed_mot.data.columns.tolist():
-        rzf          = zeroed_mot.data['ground_force2_vy']
+        rzf = zeroed_mot.data['ground_force2_vy']
         r_indexes, _ = find_peaks(-rzf, prominence=14, distance=distance, height=-100)
-        rest         = len(rzf)
-        peak         = 0
+        rest = len(rzf)
+        peak = 0
         while peak < len(r_indexes) and rest > 1000:
-            idx_start    = r_indexes[peak]
+            idx_start = r_indexes[peak]
             above_thresh = np.where(rzf[idx_start:] > threshold)[0]
             if len(above_thresh) == 0:
                 break
-            heel_idx   = idx_start + above_thresh[0]
+            heel_idx = idx_start + above_thresh[0]
             heel_contacts['R'].append(heel_idx)
             next_peaks = np.where(r_indexes > heel_idx)[0]
             if len(next_peaks) == 0:
@@ -151,16 +163,16 @@ def detect_heel_strikes(zeroed_mot, fs, threshold=20):
             rest = len(rzf[r_indexes[peak]:])
 
     if 'ground_force1_vy' in zeroed_mot.data.columns.tolist():
-        lzf          = zeroed_mot.data['ground_force1_vy']
+        lzf = zeroed_mot.data['ground_force1_vy']
         l_indexes, _ = find_peaks(-lzf, prominence=14, distance=distance, height=-100)
-        rest         = len(lzf)
-        peak         = 0
+        rest = len(lzf)
+        peak = 0
         while peak < len(l_indexes) and rest > int(fs / 2):
-            idx_start    = l_indexes[peak]
+            idx_start = l_indexes[peak]
             above_thresh = np.where(lzf[idx_start:] > threshold)[0]
             if len(above_thresh) == 0:
                 break
-            heel_idx   = idx_start + above_thresh[0]
+            heel_idx = idx_start + above_thresh[0]
             heel_contacts['L'].append(heel_idx)
             next_peaks = np.where(l_indexes > heel_idx)[0]
             if len(next_peaks) == 0:
@@ -169,6 +181,7 @@ def detect_heel_strikes(zeroed_mot, fs, threshold=20):
             rest = len(lzf[l_indexes[peak]:])
 
     return heel_contacts
+
 
 def zero_swing_phase(mot_df, toe_offs, heel_strikes, side):
     """Sets GRF and related columns to zero between toe-off and next heel strike.
@@ -212,6 +225,7 @@ def zero_swing_phase(mot_df, toe_offs, heel_strikes, side):
     mot_df.data = df_corrected
     return None
 
+
 def plot_grf_details(mot, heel_strikes, toe_offs, output, show=False):
     """Saves plot of the vertical forces with toe offs and heel strikes.
 
@@ -225,8 +239,8 @@ def plot_grf_details(mot, heel_strikes, toe_offs, output, show=False):
     """
     plt.figure(figsize=(14, 6))
     time_scale = mot.data['time'] if 'time' in mot.data.columns.tolist() else np.arange(mot.data.shape[0])
-    right_fy   = mot.data['ground_force2_vy']
-    left_fy    = mot.data['ground_force1_vy']
+    right_fy = mot.data['ground_force2_vy']
+    left_fy = mot.data['ground_force1_vy']
 
     plt.plot(time_scale, right_fy, label='Right Fy', alpha=0.7)
     plt.plot(time_scale, left_fy, label='Left Fy', alpha=0.7)
@@ -249,9 +263,45 @@ def plot_grf_details(mot, heel_strikes, toe_offs, output, show=False):
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
+    os.makedirs(output, exist_ok=True)
     plt.savefig(os.path.join(output,
                              f"{mot.filename.replace('.mot', '')}_vertical_grfs_with_toeoffs_heelstrikes.png"),
                 bbox_inches='tight')
     if show:
         plt.show()
     return None
+
+
+def segment_at_heel_strikes(mot, heel_strike_moments, mot_frame_rate=None, trc=None):
+    """Segment MOT (and matching TRC) object(s) according to heel_strikes.
+
+    Args:
+        mot                  (MOT): MOT object to process.
+        heel_strike_moments (dict): heel strikes, organized in lists by side ("R"/"L").
+        trc                  (TRC): TRC object matching the given MOT. Optional.
+        mot_frame_rate     (float): frame rate of mot_file. Optional.
+            Used when trc is not None, for faster results.
+
+    Returns:
+        dict: dictionary of the segmented objects, organized in lists by type (trc/mot) and side.
+
+    """
+    # segment mot object:
+    res = {'mot': {"Right": mot.segment(heel_strike_moments['R'])[1:-1],
+                   "Left": mot.segment(heel_strike_moments['L'])[1:-1]}}
+
+    # process trc object
+    if trc is not None:
+        trc_rate = trc.metadata['CameraRate']
+        if mot_frame_rate is None:
+            mot_frame_rate = 1 / np.mean(np.diff(mot.data['time']))
+        rate_conversion = trc_rate / mot_frame_rate
+        trc_heel_strike_moments = {}
+        for side in heel_strike_moments:
+            trc_heel_strike_moments[side] = []
+            for i in range(len(heel_strike_moments[side])):
+                trc_heel_strike_moments[side].append(int(heel_strike_moments[side][i] * rate_conversion))
+
+        res['trc'] = {"Right": trc.segment(trc_heel_strike_moments['R'])[1:-1],
+                      "Left": trc.segment(trc_heel_strike_moments['L'])[1:-1]}
+    return res
--- TreadMetrix/full_pipeline.py
+++ TreadMetrix/full_pipeline.py
@@ -4,50 +4,59 @@ import os
 from resources.filetypes_gestion.mot import MOT
 from resources.filetypes_gestion.trc import TRC
 import local_paths as local
-
 import data_postprocessing as pp
 
 if __name__ == "__main__":
-    # loads mot files:
     mot_raw_data_path = local.get_raw_mot_path()
-    mot_output        = local.get_corrected_mot_path()
-    os.makedirs(mot_output, exist_ok=True)
-    mot_file_list = sorted(f for f in os.listdir(mot_raw_data_path) if f.endswith('.mot'))
-    mot_files     = [file for file in mot_file_list if not "static" in file.lower()]
-    mots = []
-    for file in mot_files:
-        mots.append(MOT.load(mot_raw_data_path, file))
-
-    # loads trc files
     trc_raw_data_path = local.get_raw_trc_path()
-    trc_file_list = sorted(f for f in os.listdir(trc_raw_data_path) if f.endswith('.trc'))
-    trc_files = [file for file in trc_file_list if not "static" in file.lower()]
-    trcs = []
-    for file in trc_files:
-        trcs.append(TRC.load(trc_raw_data_path, file))
+    mot_corrected_output = local.get_corrected_mot_path()
 
-    segmented_mots = {}
+    # loads mot files:
+    mot_file_list = sorted(f for f in os.listdir(mot_raw_data_path) if f.endswith('.mot'))
+    mot_file_list = [file for file in mot_file_list if not "static" in file.lower()]
+    results = {}
+    for file in mot_file_list:
+        try:
+            m = MOT.load(mot_raw_data_path, file)
+        except OSError:
+            print(f"File {file} couldn't be loaded. Skipping.")
+            break
+        results[m.filename.replace('.mot', '')] = {'mot': m}
+    mot_file_list = results.keys()
 
-    # process mot files:
-    for m in mots:
-        time       = m.data['time']
+    # process files:
+    for name in mot_file_list:
+        m = results[name]['mot']
+        time = m.data['time']
         frame_rate = 1 / np.mean(np.diff(time))
         print(f"\nProcessing: {m.filename} with sampling frequency: {frame_rate:.2f} Hz.")
+        save_corrected_path = os.path.join(mot_corrected_output, name)
 
         # apply filters and baseline correction:
         pp.filter_grf(m, frame_rate)
-        pp.baseline_correct_debug(m, 'ground_force2_vy', ['ground_force2_vx', 'ground_force2_vz'], mot_output)
-        pp.baseline_correct_debug(m, 'ground_force1_vy', ['ground_force1_vx', 'ground_force1_vz'], mot_output)
-        toe_off_moments     = pp.detect_toe_offs(m, frame_rate)
+        pp.baseline_correct_debug(m, 'ground_force2_vy', ['ground_force2_vx', 'ground_force2_vz'], save_corrected_path)
+        pp.baseline_correct_debug(m, 'ground_force1_vy', ['ground_force1_vx', 'ground_force1_vz'], save_corrected_path)
+        toe_off_moments = pp.detect_toe_offs(m, frame_rate)
         heel_strike_moments = pp.detect_heel_strikes(m, frame_rate)
+        right_mot = m.copy()
         pp.zero_swing_phase(m, toe_off_moments, heel_strike_moments, 'right')
         pp.zero_swing_phase(m, toe_off_moments, heel_strike_moments, 'left')
-        m.rename(name     = m.filename.replace('.mot', '') + "_corrected",
-                 filename = m.filename.replace('.mot', '_corrected.mot'), )
-        pp.plot_grf_details(m, heel_strike_moments, toe_off_moments, mot_output)
-        m.save(mot_output)
+        m.rename(name=m.filename.replace('.mot', '') + "_corrected",
+                 filename=m.filename.replace('.mot', '_corrected.mot'), )
 
-        # segment MOT according to heel strikes
-        # segmented_mots[m.name] = m.segment(heel_strike_moments)[1:-1]
+        # pp.plot_grf_details(m, heel_strike_moments, toe_off_moments, save_corrected_path)  commented for debug
+        m.save(save_corrected_path)
 
-    print("\n All files were processed.")
+        # segment according to heel strikes:
+        trc = None
+        try:
+            trc = TRC.load(trc_raw_data_path, m.filename.replace('_corrected.mot', '.trc'))
+        except OSError:
+            print(f"No TRC file in {trc_raw_data_path} matching MOT file {name}. Skipping.")
+
+        if trc is None:
+            results[name]['segmented'] = pp.segment_at_heel_strikes(m, heel_strike_moments)
+        else:
+            results[name]['segmented'] = pp.segment_at_heel_strikes(m, heel_strike_moments, mot_frame_rate=frame_rate,
+                                                                    trc=trc)
+    print("\nAll files were processed.")
