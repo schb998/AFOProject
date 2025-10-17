2cd81dd51c941dc0dcd3a1f0a4a00bee2a581fc6
lgre690 <lilas.grenier@etu.univ-lyon1.fr>
2025-10-17 16:30:46 +1300

    updated to create directory when needed

--- TreadMetrix/local_paths.py
+++ TreadMetrix/local_paths.py
@@ -2,22 +2,24 @@ import os
 import sys
 import json
 
-with open(".local.json") as json_loc :
+with open(".local.json") as json_loc:
     LOCAL = json.load(json_loc)
 
-opensim_path      = LOCAL['opensim_path']
-model_file_name   = LOCAL['osim_model_file_name']
-base_path         = LOCAL['base_path']
+opensim_path = LOCAL['opensim_path']
+model_file_name = LOCAL['osim_model_file_name']
+base_path = LOCAL['base_path']
 
 # FILL/REPLACE WHERE NEEDED:
-frame_rate_mot       = 1000         # NUMBER OF FRAMES BY SECOND FOR MOT FILES
-frame_rate_trc       = 100          # NUMBER OF FRAMES BY SECOND FOR TRC FILES
+frame_rate_mot = 1000  # NUMBER OF FRAMES BY SECOND FOR MOT FILES
+frame_rate_trc = 100  # NUMBER OF FRAMES BY SECOND FOR TRC FILES
+
 
 def configure_opensim():
     os.environ['OPENSIM_HOME'] = opensim_path
     os.add_dll_directory(opensim_path)
     sys.path.append(os.path.join(opensim_path, 'Bindings', 'Python'))
     os.environ['PATH'] += os.pathsep + os.path.join(opensim_path, 'bin')
+
 
 def get_rates():
     """
@@ -32,36 +34,59 @@ def get_rates():
     """
     return frame_rate_mot, frame_rate_trc
 
+
 def get_base_path():
     return base_path
+
 
 def get_model_file():
     model_folder_path = LOCAL['model_folder_path'] if 'model_folder_path' in LOCAL else base_path
     return os.path.join(model_folder_path, model_file_name)
 
+
 def get_raw_mot_path():
     return LOCAL['raw_mot_path'] if 'raw_mot_path' in LOCAL else os.path.join(base_path, 'raw')
+
 
 def get_corrected_mot_path():
     return LOCAL['corrected_mot_path'] if 'corrected_mot_path' in LOCAL else os.path.join(base_path, 'corrected_mot')
 
+
 def get_segmented_mot_path():
-    return LOCAL['segmented_mot_path'] if 'segmented_mot_path' in LOCAL else os.path.join(base_path, 'segmented\\mot')
+    path = LOCAL['segmented_mot_path'] if 'segmented_mot_path' in LOCAL else os.path.join(base_path, 'segmented\\mot')
+    os.makedirs(path, exist_ok=True)
+    return path
+
 
 def get_raw_trc_path():
     return LOCAL['raw_trc_path'] if 'raw_trc_path' in LOCAL else os.path.join(base_path, 'raw')
 
+
 def get_segmented_trc_path():
-    return LOCAL['segmented_trc_path'] if 'segmented_trc_path' in LOCAL else os.path.join(base_path, 'segmented\\trc')
+    path = LOCAL['segmented_trc_path'] if 'segmented_trc_path' in LOCAL else os.path.join(base_path, 'segmented\\trc')
+    os.makedirs(path, exist_ok=True)
+    return path
+
 
 def get_external_loads_path():
-    return LOCAL['external_loads_path'] if 'external_loads_path' in LOCAL else os.path.join(base_path, 'external_loads')
+    path = LOCAL['external_loads_path'] if 'external_loads_path' in LOCAL else os.path.join(base_path, 'external_loads')
+    os.makedirs(path, exist_ok=True)
+    return path
+
 
 def get_ik_results_path():
-    return LOCAL['ik_results_path'] if 'ik_results_path' in LOCAL else os.path.join(base_path, 'ik_results')
+    path = LOCAL['ik_results_path'] if 'ik_results_path' in LOCAL else os.path.join(base_path, 'ik_results')
+    os.makedirs(path, exist_ok=True)
+    return path
+
 
 def get_id_results_path():
-    return LOCAL['id_results_path'] if 'id_results_path' in LOCAL else os.path.join(base_path, 'id_results')
+    path = LOCAL['id_results_path'] if 'id_results_path' in LOCAL else os.path.join(base_path, 'id_results')
+    os.makedirs(path, exist_ok=True)
+    return path
+
 
 def get_power_filtered_path():
-    return LOCAL['power_filtered_path'] if 'power_filtered_path' in LOCAL else os.path.join(base_path, 'power_filtered')
+    path = LOCAL['power_filtered_path'] if 'power_filtered_path' in LOCAL else os.path.join(base_path, 'power_filtered')
+    os.makedirs(path, exist_ok=True)
+    return path
