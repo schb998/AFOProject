cd9e2c5bfb5c7549cebe1310282c042999c04f86
lgre690 <lilas.grenier@etu.univ-lyon1.fr>
2025-11-11 14:32:24 +1300

    documentation

--- resources/paths/paths_gui.py
+++ resources/paths/paths_gui.py
@@ -10,38 +10,72 @@ CURRENT_ROW = 0
 
 
 def _update_labels():
+    """Update content labels of the gui.
+
+    Returns:
+        None
+    """
     for lab in LABELS:
         lab.config(text=_reformat(back.get_local(LABELS[lab])))
 
 
 def _update_row():
+    """Update current row number.
+
+    Returns:
+        None
+    """
     global CURRENT_ROW
     CURRENT_ROW = CURRENT_ROW + 1
 
 
 def _update_button():
+    """Update button activability depending on the validity of the paths required by the pipeline.
+
+    Returns:
+        None
+    """
     global BUTTON
     if BUTTON is not None:
-        result, _ = back.arevalid_loadbearing_paths(True)
+        result = back.arevalid_loadbearing_paths()
         if result is True:
             BUTTON.state(['!disabled'])
         else:
             BUTTON.state(['disabled'])
 
 
-def _reformat(string_list: list[str] | tuple[str] | str | None) -> str:
-    if string_list is None:
+def _reformat(string: list[str] | tuple[str] | str | None) -> str:
+    """Reformat given object into a comprehensible string.
+
+    Args:
+        string: object to reformat
+
+    Returns:
+        str: reformatted string
+    """
+    if string is None:
         return "empty"
-    if isinstance(string_list, str):
-        return string_list
-    length = len(string_list)
-    string = ""
+    if isinstance(string, str):
+        return string
+    length = len(string)
+    s = ""
     for i in range(length):
-        string = string + string_list[i] + "\n" if i != length - 1 else string + string_list[i]
-    return string
+        s = s + string[i] + "\n" if i != length - 1 else s + string[i]
+    return s
 
 
 def _setup_output_directory(root: Tk) -> (Label, Label, Button, Button):
+    """Set up a line on given window to manage output directory.
+
+    Args:
+        root: root Tk window
+
+    Returns:
+        Explanatory Tkinter Label of the path asked.
+        Tkinter Label containing current content of the local save.
+        Selection Tkinter Button.
+        Deselection Tkinter Button.
+    """
     label = Label(root, text="Output directory:")
     label.grid(row=CURRENT_ROW, column=0, sticky=NW, pady=10)
 
@@ -72,6 +106,17 @@ def _setup_output_directory(root: Tk) -> (Label, Label, Button, Button):
 
 
 def _setup_raw_mot(root: Tk) -> (Label, Label, Button, Button):
+    """Set up a line on given window to manage raw MOT files to process.
+
+    Args:
+        root: root Tk window
+
+    Returns:
+        Explanatory Tkinter Label of the path asked.
+        Tkinter Label containing current content of the local save.
+        Selection Tkinter Button.
+        Deselection Tkinter Button.
+    """
     label = Label(root, text="Raw MOT files to process:")
     label.grid(row=CURRENT_ROW, column=0, sticky=NW, pady=10)
 
@@ -102,6 +147,17 @@ def _setup_raw_mot(root: Tk) -> (Label, Label, Button, Button):
 
 
 def _setup_raw_trc(root: Tk) -> (Label, Label, Button, Button):
+    """Set up a line on given window to manage raw TRC files to process.
+
+    Args:
+        root: root Tk window
+
+    Returns:
+        Explanatory Tkinter Label of the path asked.
+        Tkinter Label containing current content of the local save.
+        Selection Tkinter Button.
+        Deselection Tkinter Button.
+    """
     label = Label(root, text="Raw TRC files to process:")
     label.grid(row=CURRENT_ROW, column=0, sticky=NW, pady=10)
 
@@ -131,10 +187,23 @@ def _setup_raw_trc(root: Tk) -> (Label, Label, Button, Button):
     return label, selected, select_button, unselect_button
 
 
-def _setup_row(root: Tk, label_name: str, name_in_json: str, button_txt: str, select_action, unselect_action):
-    """Do not use at the moment, not working."""
-    raise NotImplemented
+def _setup_row(root: Tk, label_name: str, name_in_json: str, select_action, unselect_action):
+    """Set up a line on given window to manage a specific path. Has issues, not to use at the moment.
 
+    Args:
+        root: root Tk window.
+        label_name: explanatory text of what is aked to the user.
+        name_in_json: name of the data to access in the path save.
+        select_action: method to call first when select button is clicked. Method should have no parameter.
+        unselect_action: method to call first when unselect button is clicked. Method should have no parameter.
+
+    Returns:
+        Explanatory Tkinter Label of the path asked.
+        Tkinter Label containing current content of the local save.
+        Selection Tkinter Button.
+        Deselection Tkinter Button.
+    """
+    raise NotImplemented
     label = Label(root, text=label_name)
     label.grid(row=CURRENT_ROW, column=0, sticky=NW, pady=10)
 
@@ -142,11 +211,22 @@ def _setup_row(root: Tk, label_name: str, name_in_json: str, button_txt: str, se
     selected.grid(row=CURRENT_ROW, column=1, sticky=NW, pady=10)
     LABELS.update({selected: name_in_json})
 
-    select_button = Button(root, text="Select", command={select_action, _update_labels()})
+    def select_button_click():
+        select_action()
+        _update_labels()
+        _update_button()
+
+    select_button = Button(root, text="Select",
+                           command=lambda: {select_button_click()})
     select_button.grid(row=CURRENT_ROW, column=2, sticky=NW, pady=10)
 
+    def unselect_button_click():
+        unselect_action()
+        _update_labels()
+        _update_button()
+
     unselect_button = Button(root, text="Unselect",
-                             command={unselect_action, _update_labels})
+                             command=lambda: {unselect_button_click()})
     unselect_button.grid(row=CURRENT_ROW, column=3, sticky=NW, pady=10)
 
     _update_row()
@@ -165,7 +245,7 @@ def _tk_window() -> Tk:
 
     _update_row()
     save_button = Button(root, text="Save to file", default='active',
-                         command=lambda: {back.save_to_json(), _update_button()})
+                         command=lambda: {back.save_to_json()})
     save_button.grid(row=CURRENT_ROW, column=0)
 
     proceed_button = Button(root, text="Proceed", default='active',
