1ac9b3f7f0d5bae037268aed9e61fee7c59d51ae
lgre690 <lilas.grenier@etu.univ-lyon1.fr>
2025-11-17 13:23:50 +1300

    minor changes

--- TreadMetrix/wip_pipeline/full_pipeline.py
+++ TreadMetrix/wip_pipeline/full_pipeline.py
@@ -1,23 +1,28 @@
 import numpy as np
 import os
 import re
+from tkinter import messagebox
+
 from resources.file_types.mot import MOT
 from resources.file_types.trc import TRC
 import resources.paths.paths_access as local
 from resources.paths.paths_gui import main as gui_main
 import data_postprocessing as pp
-from tkinter import messagebox
+import osim_gestion as osim
+import ik_data as ik
 
 
 if __name__ == "__main__":
     # update local paths and read them:
     gui_main()
+    osim.configure_opensim()
+
     raw_mot_files = local.get_raw_mot_path()
     raw_trc_files = local.get_raw_trc_path()
     mot_corrected_output = local.get_corrected_mot_path()
 
-    save = messagebox.askokcancel("Save optional files", "Should the optional files be saved?")
-    show = messagebox.askokcancel("Show plots when running", "Should the plots be shown on screen during processing?")
+    save = messagebox.askokcancel("Save optional files", "Save optional files")
+    show = messagebox.askokcancel("Show plots when running", "Show plots on screen during processing")
 
     # loads files:
     results = {}
@@ -52,25 +57,24 @@ if __name__ == "__main__":
         m.rename(name=m.filename.replace('.mot', '') + "_corrected",
                  filename=m.filename.replace('.mot', '_corrected.mot'), )
 
-        pp.plot_grf_details(m, heel_strike_moments, toe_off_moments, str(save_corrected_path), show=show)
+        # pp.plot_grf_details(m, heel_strike_moments, toe_off_moments, str(save_corrected_path), show=show)
         m.save(save_corrected_path)
 
         # segment according to heel strikes:
         trc = None
-        trc_name = name + "\\.trc$"
-
+        trc_name_regex = name + "\\.trc$"
         trc_found = False
 
         try:
-            matching_trc = [t for t in raw_trc_files if re.search(trc_name, t) is not None][0]
+            matching_trc = [t for t in raw_trc_files if re.search(trc_name_regex, t) is not None][0]
             trc_found = True
         except IndexError:
             print(f"No selected TRC file matching MOT file {name}. Skipping.")
             matching_trc = None
 
-
         if trc_found:
             try:
+                TRC.adapt_to_opensim_use(matching_trc)
                 trc = TRC.load(matching_trc)
                 results[name]['segmented'] = pp.segment_at_heel_strikes(m, heel_strike_moments, mot_frame_rate=frame_rate,
                                                                         trc=trc, save=save)
@@ -80,5 +84,9 @@ if __name__ == "__main__":
 
         if not trc_found:
             results[name]['segmented'] = pp.segment_at_heel_strikes(m, heel_strike_moments, save=save)
+
+        else:
+            print("IK postprocessing part fo the pipeline is still a WIP.")
+            # ik.process(results[name]['segmented']['trc'], name)
 
     print("\nAll files were processed.")
